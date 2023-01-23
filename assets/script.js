6ff06699f35661324c0b3c697d3c414854dcab52
Sho.ayb <Sho.ayb@outlook.com>
2023-01-23 16:51:37 +0000

    daily planner html fixed and object schedule created

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,103 @@
-// lets add the date from moment library in to the jumbotron
+/*
 
-// lets query select the paragraph element in the header to insert the date
+- when the user opens the browser, will be presented with a header reading - Work Day Scheduler, a short paragraph and the current date in the format of day of week, month and day of month e.g Monday, January 23. 
 
-// const dateEl = $("#currentDay");
+- there will be a scheduler displaying the working hours of the day - 9am to 5pm, a text area allows for the user to add a schedule and this schedule can be saved to localStorage by clicking on the icon on the far right of each time-block. Saved schedules should remain persistent even after a browser refresh. 
 
-// console.log(dateEl);
 
-// const now = moment();
+- when the user opens the browser and views the scheduler - the current time of the day will be coloured different from past and future time-blocks. This present time-block should display a message in the description advising "Current time of day". 
 
-// const displayTime = () => {
-//   const currentDate = now.format("dddd Do MMM YYYY");
+- this means that we need to use momentjs to return the current time - and with this current time we can work out the past, present and future time-blocks. 
 
-//   dateEl.text(currentDate);
-// };
+- in order to accomplish the above and to save each time-block to localStorage - we shall order each time-block within an array of objects. 
 
-// displayTime();
+  - The following array of objects is indicated below: 
+
+    const schedule = [
+
+      {
+
+        time: 9, 
+        description: ''
+
+      }, 
+
+      {
+
+        time: 10, 
+        description: ''
+      }, 
+
+      {
+
+        time: 11,
+        description: ''
+
+      }
+
+    ]
+
+This way we can loop through each object and match the time property of the object against the current time and then apply a background color to each element in question based on past, present and future times. 
+
+    - we can achieve this by simply saying: if schedule.time is less than current time then change element(s) to a different background color or if schedule.time is greater than current time then change element(s) to a different background color. 
+
+Additional
+
+When the user clicks on save button - the buttons icon should change to another icon showing the user can delete the schedule created for that time-block. Once clicked the schedule should be removed from localStorage. 
+
+
+
+*/
 
 // lets ensure that the DOM has been fully loaded before html is manipulated
 
 $(function () {
   console.log("DOM has been loaded");
 
+  // lets create an array of objects to store all the schedules
+
+  const schedule = [
+    {
+      time: 9,
+      description: "",
+    },
+    {
+      time: 10,
+      description: "",
+    },
+    {
+      time: 11,
+      description: "",
+    },
+    {
+      time: 12,
+      description: "",
+    },
+    {
+      time: 1,
+      description: "",
+    },
+    {
+      time: 2,
+      description: "",
+    },
+    {
+      time: 3,
+      description: "",
+    },
+    {
+      time: 4,
+      description: "",
+    },
+    {
+      time: 5,
+      description: "",
+    },
+  ];
+
   // lets create a variable to store the date from momentjs
 
   const now = moment();
-
-  console.log(now.format("dddd, MMMM D"));
 
   // lets create a function to display the date
 
