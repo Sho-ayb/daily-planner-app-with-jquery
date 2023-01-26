e942444fab7fb1036500a3063b2955fb54f97514
Sho.ayb <Sho.ayb@outlook.com>
2023-01-26 13:25:34 +0000

    data is now being stored correctly to local storage for each textarea the user inserts data to

--- assets/script.js
+++ assets/script.js
@@ -11,21 +11,6 @@ $(document).ready(function () {
 
   const now = moment();
 
-  $(".saveBtn").on("click", (e) => {
-    e.preventDefault();
-    var id = $(e.target).siblings("textarea").attr("id");
-    var storedData = $(e.target).siblings("textarea").val();
-
-    console.log("id", id, "stored data", storedData, "this", $(this));
-    console.log(e.target);
-    localStorage.setItem(id, storedData);
-  });
-
-  for (let i = 9; i < 18; i++) {
-    var storedLocal = localStorage.getItem(i);
-    $("#" + i).val(storedLocal);
-  }
-
   // lets create a display date function here
 
   const displayDate = () => {
@@ -34,14 +19,54 @@ $(document).ready(function () {
     $("#currentDay").text(date);
   };
 
+  // lets create a function here to add a schedule to local storage
+
+  const addSchedule = () => {
+    $(".saveBtn").on("click", (e) => {
+      // lets prevent the page reloading after each button click
+
+      e.preventDefault();
+
+      // lets create a variable here to store all the previous and next data recieved to local storage when the user clicks on the button
+
+      // Note: if we simply create an empty array to push to it, i have noted that local storage will be cleared and then overwritten with the schedule data of the textarea the user wants to save, this is why we need to parse the current data stored from local storage here, then below will save the next append to the previous schedules already stored
+
+      const scheduleRecord = window.localStorage.getItem("schedule")
+        ? JSON.parse(window.localStorage.getItem("schedule"))
+        : [];
+
+      // lets extract the id and the value from the textarea
+      var id = $(e.target).siblings("textarea").attr("id");
+      var storedData = $(e.target).siblings("textarea").val();
+
+      console.log("id", id, "stored data", storedData);
+      console.log(e.target);
+
+      // lets push these vars to scheduleRecord before storing them to local storage
+
+      scheduleRecord.push({ id, storedData });
+
+      // lets now save this to local storage
+      window.localStorage.setItem("schedule", JSON.stringify(scheduleRecord));
+    });
+
+    // for (let i = 9; i < 18; i++) {
+    //   var storedLocal = localStorage.getItem(i);
+    //   $("#" + i).val(storedLocal);
+    // }
+  };
+
+  // lets create a function here to display all the data stored in local storage to each text area in question
+
+  const displayAllSchedules = () => {
+    // lets first retrieve all the data stored from local storage
+    // now that we have the data from local storage, let loops through parsedStorage
+    // and display each data in the textarea
+  };
+
   // lets create a function to style the textareas to appear with a different background color based on the current time
 
   const changeTextArea = () => {
-    // lets create two empty arrays to store which textarea with the ids less than the current time and textareas with the ids greater than the current time
-
-    // const prevTimes = [];
-    // const futureTimes = [];
-
     const currentTime = now.format("H");
 
     console.log("current time: ", Number.parseInt(currentTime));
@@ -51,14 +76,9 @@ $(document).ready(function () {
 
     const txtarea = $("textarea");
 
-    console.log(txtarea);
-    console.log(txtarea.attr("id"));
-
     // note: i found that not converting the both id and the current time to a number produces unexpect result and did not match the condition correctly. Now converted to Number integer and background color is being set correctly on the specific textareas past, present and future times
 
     $.each(txtarea, function (key, area) {
-      console.log(area);
-
       // the present time
       if (Number.parseInt(area.id) === Number.parseInt(currentTime))
         $(area).css("background-color", "red");
@@ -78,6 +98,8 @@ $(document).ready(function () {
   const init = () => {
     displayDate();
     changeTextArea();
+    addSchedule();
+    displayAllSchedules();
   };
 
   // lets invoke the init function
